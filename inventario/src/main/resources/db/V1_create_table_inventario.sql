58bd06c4cf551fdc1cb23b54e23865f9691c7661
Sebastiana-pablaza <se.apablazap@duocuc.cl>
2026-05-09 21:13:57 -0400

    commit

--- inventario/src/main/resources/db/V1_create_table_inventario.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE inventario (
-    id            BIGINT   NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    producto_id   BIGINT   NOT NULL UNIQUE,
-    cantidad      INT      NOT NULL DEFAULT 0,
-    ultima_actualizacion DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
