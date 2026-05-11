f300f8b202307764214836bf2e0112ee05ad2f2d
Sebastiana-pablaza <se.apablazap@duocuc.cl>
2026-05-10 20:15:04 -0400

    commit

--- pagos/src/main/java/cl/duoc/pagos/model/Pagos.java
+++ pagos/src/main/java/cl/duoc/pagos/model/Pagos.java
@@ -3,6 +3,7 @@ package cl.duoc.pagos.model;
 import java.sql.Date;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -11,7 +12,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+@Entity
 @Data
 @Table(name= "pagos")
 @AllArgsConstructor
--- usuario/src/main/java/cl/duoc/usuario/model/Usuario.java
+++ usuario/src/main/java/cl/duoc/usuario/model/Usuario.java
@@ -1,6 +1,7 @@
 package cl.duoc.usuario.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -10,6 +11,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 @Table(name = "usuario")
 @Builder
