ca037bf13867363b6ef01c66518d56c0aeb5241e
Sebastiana-pablaza <se.apablazap@duocuc.cl>
2026-05-17 01:05:43 -0400

    se borraron las bases de datos con contenido duplicado (boilerplate)

--- pagos/src/main/resources/V2__initial_data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO pagos (id_orden, metodo_pago, monto_a_pagar, monto_pagado, fecha_transaccion, estado_pago)
-VALUES 
-(1, 'Credito', 45000.00, 45000.00, '2026-05-15 14:30:00', 'Realizado'),
-(2, 'Transferencia', 12500.00, 0.00, '2026-05-16 09:15:00', 'Pendiente'),
-(3, 'Debito', 8990.00, 0.00, '2026-05-16 10:00:00', 'Rechazado');
